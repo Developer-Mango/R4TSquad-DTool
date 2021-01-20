5e53af8e4f562637b761959b3bedf94b6a420a37
Mango <54734106+Developer-Mango@users.noreply.github.com>
2021-01-20 18:47:17 +1030

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -86,8 +86,8 @@ namespace R4TSquad_DTool
                         }
                     }
                 }
-                Console.ReadLine();
             }
+            Console.ReadLine();
         }
     }
 }
